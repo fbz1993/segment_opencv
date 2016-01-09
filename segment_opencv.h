3c3c5d219d6b53aade21e0cb742f70df1947241b
naoki <naoki@naoki-air.local>
2016-01-09 19:57:09 +0900

    Doxygenコメントを追加

--- segment_opencv.h
+++ segment_opencv.h
@@ -1,13 +1,20 @@
+/**
+ * @file  segment_opencv.h
+ * @brief Wrapper of Felzenszwalb's graph-based segmentation for OpenCV
+ */
+
 #ifndef CV_SEGMENT_H
 #define CV_SEGMENT_H
 
-#include <image.h>
-#include <misc.h>
 #include <opencv2/opencv.hpp>
 
-// Mat -> image<rgb>*
-image<rgb>* convertfromMat(const cv::Mat& src_img);
-
+/**
+ * @brief Graph-based segmentation.
+ * @param src_img Input image is CV_8UC3.
+ * @param sigma,c,min_size Parameters of segmentation.
+ * @param num_ncc The number of output segments.
+ * @param dst_img Output image is CV_32SC1. dst_img.at<int>(i) is index of segment of pixel i.
+ */
 void segment_image(const cv::Mat& src_img, float sigma, float c, int min_size, int *num_ccs, cv::Mat& dst_img);
 
 #endif
